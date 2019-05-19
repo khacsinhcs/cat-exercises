bf510397e5f386f565e26a55ec782159aedef8b8
Sinh Le <khacsinhcs@gmail.com>
2019-05-19 19:14:08 +0700

    lession to learn: There are 2 ways for implement a Json syntax

--- src/test/scala/com/alab/cat/JsonWriterSpec.scala
+++ src/test/scala/com/alab/cat/JsonWriterSpec.scala
@@ -18,16 +18,33 @@ class JsonWriterSpec extends WordSpec with Matchers {
     def write(value: A): Json
   }
 
-  object Json {
-    def toJson[A](value: A)(implicit jsonWriter: JsonWriter[A]) = jsonWriter.write(value)
-  }
 
   "It should be toJson with Json singleton Object" in {
+    object Json {
+      def toJson[A](value: A)(implicit jsonWriter: JsonWriter[A]) = jsonWriter.write(value)
+    }
+
     import JsonWriterInstances._
     //We have to import implicit value personWriter in JsonWriterInstances
     val person = Person("Sinh", "khacsinhcs@gmail.com")
     Json.toJson(person)
     Json.toJson("Hello world") should be(JsString("Hello world"))
+  }
+
+  "This is the time to learn about json syntax" in {
+    object JsonSyntax {
+
+      implicit class JsonWriterOps[A](value: A) {
+        def toJson(implicit w: JsonWriter[A]) = w.write(value)
+      }
+
+    }
+    import JsonWriterInstances._
+    import JsonSyntax._
+    val person = Person("Sinh", "khacsinhcs@gmail.com")
+
+    "This is Json".toJson should be(JsString("This is Json"))
+    person.toJson
   }
 
   object JsonWriterInstances {
