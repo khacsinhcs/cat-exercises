a17737c7b315d1606848d1ce9e70dd3f5e171483
Sinh Le <khacsinhcs@gmail.com>
2019-05-19 19:05:20 +0700

    add Json object

--- src/test/scala/com/alab/cat/JsonWriterSpec.scala
+++ src/test/scala/com/alab/cat/JsonWriterSpec.scala
@@ -18,6 +18,18 @@ class JsonWriterSpec extends WordSpec with Matchers {
     def write(value: A): Json
   }
 
+  object Json {
+    def toJson[A](value: A)(implicit jsonWriter: JsonWriter[A]) = jsonWriter.write(value)
+  }
+
+  "It should be toJson with Json singleton Object" in {
+    import JsonWriterInstances._
+    //We have to import implicit value personWriter in JsonWriterInstances
+    val person = Person("Sinh", "khacsinhcs@gmail.com")
+    Json.toJson(person)
+    Json.toJson("Hello world") should be(JsString("Hello world"))
+  }
+
   object JsonWriterInstances {
     implicit val stringWriter: JsonWriter[String] = (value: String) => JsString(value)
     implicit val numberWriter: JsonWriter[Double] = (value: Double) => JsNumber(value)
